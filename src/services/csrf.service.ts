d97eac641531065ea779f3a85985aa900223d7e5
Dany-py <danydegbe@gmail.com>
2026-04-21 21:55:40 +0000

    Update initCSRF

--- src/services/csrf.service.ts
+++ src/services/csrf.service.ts
@@ -25,8 +25,10 @@ export function getCookie(name: string): string {
 export async function initCSRF() {
     const url = import.meta.env.VITE_API_CSRF as string;
     const response = await apiClient.get(url);
+    console.log('Document cookie :', document.cookie)
     const token = response.data.csrfToken;
     document.cookie = `csrftoken=${token}`
+    console.log('New document cookie :', document.cookie)
     
     // Stocker dans axios pour toutes les requêtes suivantes
     apiClient.defaults.headers.common['X-CSRFToken'] = token;
